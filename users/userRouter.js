c6db7792a6a3c3e930ea8aa9f8a02c6d5966e4fc
Ethan Hoover <51389138+Cireimu@users.noreply.github.com>
2020-02-04 13:36:31 -0800

    added a get all users router for testing reasons

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const Reviews = require('../reviews/reviewModel');
+const Users = require('./userModel');
 
 const { checkUserId } = require('./validation-middleware');
 
@@ -16,4 +17,16 @@ router.get('/:username/reviews', checkUserId, (req, res) => {
     .catch(err => res.status(500).json({ message: 'Failed to get reviews from database', error:err}))
 })
 
+router.get('/', (req, res) => {
+
+    Users.findAllUsers()
+    .then(users => {
+        if(users.length > 0) {
+            res.status(200).json(users)
+        } else {
+            res.status(404).json({message: 'Doesnt appear as though there are users in database'})
+        }
+    })
+    .catch(err => res.status(500).json({ message: 'failed to get users from database'}))
+})
 module.exports = router;
