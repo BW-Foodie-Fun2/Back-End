db7a25cd13fa00030e3a215734be6758d1933348
Ethan Hoover <51389138+Cireimu@users.noreply.github.com>
2020-02-05 08:04:12 -0800

    added server autowake up into index.js

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
     console.log(`\n** server listening on port ${PORT} **\n`)
 });
+
+var https = require("https");
+setInterval(function() {
+    https.get("https://foodiefunbw.herokuapp.com/");
+}, 1000 * 60 * 5); // every 5 minutes (300000)
