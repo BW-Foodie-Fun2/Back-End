519184c0a52b8ee09a5b091c7f64a9eeb3fff8ad
Ethan Hoover <51389138+Cireimu@users.noreply.github.com>
2020-02-10 09:08:00 -0800

    updated the restaurant routers as I said I would

--- api/restaurants/restaurantModel.js
+++ api/restaurants/restaurantModel.js
@@ -1,13 +1,20 @@
 const db = require('../../database/db-config');
 
 module.exports = {
-  addRestaurant,
+  findBy,
+  add,
   findRestaurants,
   findById,
-  deleteRestaurant,
-  updateRestaurant,
+  remove,
+  update,
   findByUsername
 };
+
+function findBy(filter) {
+  return db('restaurants')
+    .where(filter)
+    .first()
+}
 
 function findRestaurants() {
   return db('restaurants as r')
@@ -16,29 +23,34 @@ function findRestaurants() {
 }
 
 
-function addRestaurant(restaurant) {
+function add(restaurant) {
+  console.log("restaurantModel add(): ", JSON.stringify(restaurant))
   return db('restaurants')
     .insert(restaurant)
+    .returning('id')
     .then(ids => {
       const [id] = ids;
       return findById(id);
     })
 }
 
-function findById(id) {
-  return db('restaurants')
-    .where('restaurants.id', id)
+function findById(restaurant_id) {
+  return db('restaurants as r')
+    .where('r.id', restaurant_id)
+    .join('cuisines as c', 'r.cuisine_id', 'c.id',)
+    .select('r.id as id', 'r.name as name', 'c.cuisine_name as cuisine', 'r.location as location', 'r.hours_of_operation as hours_of_operation', 'r.img_url as img_url', 'r.created_by as created_by', 'r.created_at as created_at', 'r.updated_at as updated_at')
+    .first()
 }
 
-function updateRestaurant(id, changes) {
+function update(id, changes) {
   return db('restaurants')
-    .where('restaurants.id', id)
-    .update(changes)
+    .where({ id })
+    .update(changes);
 }
 
-function deleteRestaurant(id) {
+function remove(id) {
   return db('restaurants')
-    .where('restaurants.id', id)
+    .where('id', id)
     .del();
 }
 
@@ -46,6 +58,6 @@ function findByUsername(username) {
   return db('restaurants as r')
     .where({ created_by: username })
     .join('cuisines as c', 'r.cuisine_id', 'c.id')
-    .select('r.id', 'r.name', 'c.cuisine_name', 'r.location', 'r.hours_of_operation', 'r.img_url', 'r.created_by')
+    .select('r.id', 'r.name', 'c.cuisine_name', 'r.location', 'r.hours_of_operation', 'r.img_url')
     .orderBy('r.id')
 }
